8326972db6631d747bc3a27645733b7213934d5a
IgorEverton <rm95214@fiap.com.br>
2023-02-19 22:38:11 -0300

    Layout e bamco fake(Json) cirado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import Menu from "./componentes/Menu/menu";
+import BannerMain from "./components/BannerMain";
+import Menu from "./components/Menu/menu";
+import dadosIniciais from './data/dados_iniciais.json'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
+import styled from 'styled-components'
+
+
+const AppWrapper = styled.div`
+  background: #141414;
+
+`
 
 function App() {
   return (
-    <div>
+    <AppWrapper>
       <Menu/>
-      18-02-2023
-    </div>
+      <BannerMain
+      videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+      url={dadosIniciais.categorias[0].videos[0].url}
+      videoDescription={"IESAFLIX é tipo Netflix só que melhor "}
+      />
+      <Carousel
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+      category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+      category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel
+      category={dadosIniciais.categorias[3]}
+      /> 
+
+      <Carousel
+      category={dadosIniciais.categorias[4]}
+      /> 
+
+      <Carousel
+      category={dadosIniciais.categorias[5]}
+      />
+      <Footer/>
+    </AppWrapper>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+function CadastroVideo(){
+  return(
+    <>
+      Página de Cadastro de Vídeo
+    </>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/"element={<App/>} />
+      <Route path="/cadastro/video"element={<CadastroVideo/>} />
+    </Routes>   
+  </BrowserRouter>
 );
 
