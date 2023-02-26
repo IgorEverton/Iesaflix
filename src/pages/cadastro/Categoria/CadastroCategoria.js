6085889afa55ee335807315b3b9f8eec00a85b5f
IgorEverton <rm95214@fiap.com.br>
2023-02-25 21:11:14 -0300

    estruturando o formulario com onChance e onSubmit

--- src/pages/cadastro/Categoria/CadastroCategoria.js
+++ src/pages/cadastro/Categoria/CadastroCategoria.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import RouterPages from '../../../RoutePages/RouterPages';
 
 
 export default function CadastroCategoria(){
-    return(
+  
+
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Nova categoria')
+  const [novaCategorias, setNovaCategoria] = useState(['teste'])
+  
+  return(
       <RouterPages>
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
         
+        <form onSubmit={function submeterForme(e){
+          e.preventDefault()
+          setNovaCategoria([
+            ...nomeDaCategoria
+          ])
+          
+        }}>
+
+          <label>
+              Nome da Categoria:
+            <input
+            type="text"
+            value={nomeDaCategoria}
+            onChange={function pegarValue(e){
+              setNomeDaCategoria(e.target.value)
+            }}
+            />
+          </label>
+
+          <button>
+            Cadastrar
+          </button>
+      </form>
+            <ul>
+                {novaCategorias.map((categorias, i) => {
+                  return(
+                    <li key={categorias+i }>
+                      
+                      {categorias}
+                    </li>
+                  )
+                })}
+            </ul>
+
+
+
+
         <Link to="/">
           Ir para o Home
         </Link>
